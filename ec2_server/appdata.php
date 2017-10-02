46bf033f3a8acf5d2d6cd0c8815239dbdeead149
DESKTOP-UCJTCLE\yesol <lotrob88@gmail.com>
2017-10-02 01:16:30 -0700

    environment build up

---
+++ ec2_server/appdata.php
@@ -0,0 +1,79 @@
+<?php  
+error_reporting(E_ALL); 
+ini_set('display_errors',1); 
+
+$con=mysqli_connect("csci150.c9g7nukf2ffx.us-west-1.rds.amazonaws.com","root","1234qwer","CSCI150"); 
+if (!$con)  
+{ 
+   echo "MySQL Connection Error : ";
+   echo mysqli_connect_error();
+   exit();
+}  
+
+mysqli_set_charset($con,"utf8");  
+
+//POST 값을 읽어온다.
+$name=isset($_POST['name']) ? $_POST['name'] : '';  
+$email=isset($_POST['email']) ? $_POST['email'] : '';  
+$pswd=isset($_POST['pswd']) ? $_POST['pswd'] : '';  
+
+$query = "SELECT * FROM user_db WHERE email = '{$email}'";
+$res = mysqli_query($con, $query);
+$res_num = mysqli_num_rows($res);
+
+if ($name !="" and $email !="" and $pswd !="" ){   
+  if($res_num != 0) {
+    echo "Email Already Exist!";
+  }else {
+      $sql = "SELECT PASSWORD('{$pswd}')";
+      $res = mysqli_query($con, $sql);
+      var_dump($res);
+
+      exit;
+
+      $sql = 
+        "INSERT INTO 
+          user_db(name, email, pswd) 
+          VALUES('$name', '$email', '$pswd')";
+
+      $res = mysqli_query($con, $sql);
+
+      if($res){  
+         echo "SUCCESS";  
+      }  
+      else{  
+         echo "ERROR - SQL : "; 
+         echo mysqli_error($con);
+      } 
+
+  } 
+} else {
+    echo "데이터를 입력하세요 ";
+}
+
+
+mysqli_close($con);
+?>
+
+<?php
+
+$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+
+if (!$android){
+?>
+
+<html>
+   <body>
+   
+      <form action="<?php $_PHP_SELF ?>" method="POST">
+         Name: <input type = "text" name = "name" />
+         Email: <input type = "text" name = "email" />
+         Password: <input type = "text" name = "pswd" />
+         <input type = "submit" />
+      </form>
+   
+   </body>
+</html>
+<?php
+}
+?>
