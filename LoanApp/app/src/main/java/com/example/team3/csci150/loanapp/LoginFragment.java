c8caffe3c0a3fa349766f8c5362ab435cadd7a23
DESKTOP-UCJTCLE\yesol <lotrob88@gmail.com>
2017-10-10 02:01:29 -0700

    activity to fragment

---
+++ LoanApp/app/src/main/java/com/example/team3/csci150/loanapp/LoginFragment.java
@@ -0,0 +1,129 @@
+package com.example.team3.csci150.loanapp;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.channels.AsynchronousCloseException;
+
+public class LoginFragment extends TopLevelFragment {
+    EditText input_email, input_pwd;
+    String email,pwd;
+    TextView result;
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        input_email = (EditText) view.findViewById(R.id.input_email);
+        input_pwd = (EditText) view.findViewById(R.id.input_pwd);
+
+        view.findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //login success
+                LoginProcess lp = new LoginProcess();
+                lp.execute("http://52.53.157.82/csci150/php/login.php",email,pwd);
+            }
+        });
+
+        return view;
+    }
+
+    class LoginProcess extends AsyncTask<String, String, String> {
+        ProgressDialog createProgress;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            createProgress = ProgressDialog.show(getActivity(), "Loading...", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            createProgress.dismiss();
+//            result.setText(s);
+            if(s.equals("SUCCESS")) {
+                getMainActivity().resetToplevel(LoginFragment.newInstance());
+            }
+            Log.d("RESPONSE", "POST response - " + result);
+        }
+        @Override
+        protected String doInBackground(String... params) {
+
+            String server = (String)params[0];
+            String email = (String)params[1];
+            String pswd = (String)params[2];
+
+//            String server = "http://52.53.157.82/csci150/php/login.php";
+            String postParams = "email=" + email + "&pswd=" + pswd;
+
+            try {
+                java.net.URL url = new URL(server);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(4000);
+                httpURLConnection.setConnectTimeout(4000);
+                httpURLConnection.setRequestMethod("POST");
+                //httpURLConnection.setRequestProperty("content-type", "application/json");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParams.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responCode = httpURLConnection.getResponseCode();
+                Log.d("RESPONSE", "POST response code - " + responCode);
+                InputStream inputStream;
+                if(responCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }else {
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString();
+
+            } catch (Exception e) {
+                Log.d("RESPONSE", "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
+}
---
+++ php/login.php
@@ -0,0 +1,65 @@
+<?php
+
+$fs = fopen(dirname(__FILE__)."/manager.txt", "r");
+$mode = trim(fread($fs, "1024"));
+if($mode == "") $mode = "dev";
+fclose($fs);
+
+//echo $mode;
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+if($mode == "dev") {
+  $con = mysqli_connect("localhost:3306","root","1234qwer","csci150");
+}else {
+  $con = mysqli_connect("csci150.c9g7nukf2ffx.us-west-1.rds.amazonaws.com","root","1234qwer","CSCI150");
+}
+
+if (!$con)
+{
+   echo "MySQL Connection Error : ";
+   echo mysqli_connect_error();
+   exit();
+}
+
+mysqli_set_charset($con,"utf8");
+
+$email=isset($_POST['email']) ? $_POST['email'] : '';
+$pswd=isset($_POST['pswd']) ? $_POST['pswd'] : '';
+
+$query = "SELECT * FROM user_db
+         WHERE email = '{$email}' AND pswd = '{$pswd}'";
+$res = mysqli_query($con, $query);
+
+if($res){
+  echo "SUCCESS";
+}
+else{
+  echo "ERROR - SQL : ";
+  echo mysqli_error($con);
+}
+
+mysqli_close($con);
+?>
+
+<?php
+
+$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
+
+if (!$android){
+?>
+
+<html>
+   <body>
+
+      <form action="<?php $_PHP_SELF ?>" method="POST">
+         Email: <input type = "text" name = "email" />
+         Password: <input type = "text" name = "pswd" />
+         <input type = "submit" />
+      </form>
+
+   </body>
+</html>
+<?php
+}
+?>
