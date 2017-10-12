07034c0b6e2bdb0b4718b7af111fcde48a5b394e
DESKTOP-UCJTCLE\yesol <lotrob88@gmail.com>
2017-10-12 00:03:58 -0700

    db connection info modify

--- php/login.php
+++ php/login.php
@@ -12,7 +12,7 @@ ini_set('display_errors',1);
 if($mode == "dev") {
   $con = mysqli_connect("localhost:3306","root","1234qwer","csci150");
 }else {
-  $con = mysqli_connect("csci150.c9g7nukf2ffx.us-west-1.rds.amazonaws.com","root","1234qwer","CSCI150");
+  $con = mysqli_connect("csci150se.c9g7nukf2ffx.us-west-1.rds.amazonaws.com","intrSE_3","tkfkdgo11","SPLITTR");
 }
 
 if (!$con)
@@ -27,21 +27,17 @@ mysqli_set_charset($con,"utf8");
 $email=isset($_POST['email']) ? $_POST['email'] : '';
 $pswd=isset($_POST['pswd']) ? $_POST['pswd'] : '';
 
-$query = "SELECT * FROM user_db
+$query = "SELECT * FROM user
          WHERE email = '{$email}' AND pswd = '{$pswd}'";
 $res = mysqli_query($con, $query);
-$res_num = mysqli_num_rows($res);
 
 if($email != "" and $pswd != "") {
-  if($res_num> 0){
-    while($row = mysqli_fetch_assoc($res)) {
-      echo "SUCCESS";
-    }
-  }
-  else{
+  if(mysqli_num_rows($res) == 0) {
     echo "Wrong email or password!";
+  }else {
+    while($row = mysqli_fetch_assoc($res)) echo "SUCCESS";
   }
-} else {
+}else {
   echo "Please input data";
 }
 
