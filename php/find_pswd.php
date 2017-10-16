f53ef2cc4fbda60fbf3f1342ac23a322aa55e467
DESKTOP-UCJTCLE\yesol <lotrob88@gmail.com>
2017-10-16 00:11:42 -0700

    -f

--- php/find_pswd.php
+++ php/find_pswd.php
@@ -69,11 +69,8 @@ if($email != "") {
 		</body>
 	</html>
 	";
-
-	$fullmsg = "From: webmaster@splittr.com\nTo: {$email}\nSubject: {$message}";
-
-	//mail($email, "Your temporary password", $message, $headers);
-	exec("echo $fullmsg | /usr/sbin/sendmail -f webmaster@splittr.com {$email}");
+	$from = "noreply@splittr.com"
+	mail($email, "Your temporary password", $message, $headers, "-f" . $from);
   }
 }else {
   echo "Please input data";
