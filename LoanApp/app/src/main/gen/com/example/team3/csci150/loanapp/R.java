be02a1c7b7c7f547a68813f40e5ed2fef90e59a8
SALEH-PC\Owner <salih747@mail.fresnostate.edu>
2017-11-09 15:25:33 -0800

    no message

---
+++ LoanApp/app/src/main/gen/com/example/team3/csci150/loanapp/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.team3.csci150.loanapp;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
